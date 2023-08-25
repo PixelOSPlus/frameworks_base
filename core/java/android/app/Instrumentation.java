9bcca2ee7ec3f00a347997f7e9896faededaa0db
Gustavo Mendes <gusttavo.me@outlook.com>
2023-08-24 22:25:32 -0300

    GamesPropsUtils: Use app context instead of package name
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1285,10 +1285,9 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        String packageName = context.getPackageName();
         PropImitationHooks.setProps(context);
         MeizuPropsUtils.setProps(app);
-        GamesPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(app);
         return app;
     }
     
@@ -1306,10 +1305,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        String packageName = context.getPackageName();
         PropImitationHooks.setProps(context);
         MeizuPropsUtils.setProps(app);
-        GamesPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(app);
         return app;
     }
 
