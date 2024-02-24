2c9959db2a4cae10ea9760bcc291112f5e09497b
QKIvan <ivan33@163.com>
2024-02-24 16:35:01 +0100

    Revert "Revert "SystemUI: Add haptic feedback to auto brightness icon""
    This reverts commit f106afc0f778fbc700a365c3592190434ecd692a.

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -111,6 +111,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private ValueAnimator mSliderAnimator;
 
     private Vibrator mVibrator;
+
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
 
@@ -339,6 +340,15 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL :
                     Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
                 UserHandle.USER_CURRENT));
+
+        mIcon.setOnClickListener(v -> {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE, mAutomatic ?
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL :
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
+                    UserHandle.USER_CURRENT);
+            if (mVibrator != null) mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+        });
     }
 
     public void registerCallbacks() {
